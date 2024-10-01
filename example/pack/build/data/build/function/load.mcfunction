f8d8c072b0d3aef4708ce0ee885ee71473ac226c
Oein <oein0219@gmail.com>
2024-10-01 12:17:12 +0900

    add base

--- example/pack/build/data/build/function/load.mcfunction
+++ example/pack/build/data/build/function/load.mcfunction
@@ -1,4 +1,7 @@
 # This data pack was compiled with the 40planet's compiler.
 # https://github.com/alexmonkey05/Datapack-Compiler
 
-say Hello, World!
+data modify storage 40planet:value data.build_var_temp1 set from storage 40planet:value data.build_temp1
+execute store result storage 40planet:value data.build_temp2 int 1 run data get storage 40planet:value data.build_var_temp1
+tellraw @a [{"nbt":"data.build_temp2","storage":"40planet:value"}]
+tellraw @a [{"text":"Hi"}]
--- planet/vocs.config.ts
+++ planet/vocs.config.ts
@@ -16,8 +16,46 @@ export default defineConfig({
       link: "/del-done",
     },
     {
-      text: "변수",
-      link: "/variable",
+      text: "문법",
+      collapsed: true,
+      items: [
+        {
+          text: "변수",
+          link: "/syntax/variable",
+        },
+        {
+          text: "배열",
+          link: "/syntax/array",
+        },
+        {
+          text: "매크로",
+          link: "/syntax/macro",
+        },
+        {
+          text: "연산",
+          link: "/syntax/calculation",
+        },
+        {
+          text: "주석",
+          link: "/syntax/comment",
+        },
+        {
+          text: "조건문",
+          link: "/syntax/if",
+        },
+        {
+          text: "반복문",
+          link: "/syntax/while",
+        },
+        {
+          text: "함수",
+          link: "/syntax/function",
+        },
+        {
+          text: "마인크래프트 명령어",
+          link: "/syntax/minecraft",
+        },
+      ],
     },
     {
       text: "내장 함수",
@@ -26,6 +64,56 @@ export default defineConfig({
         {
           text: "print",
           link: "/built-in/print",
+        },
+        {
+          text: "random",
+          link: "/built-in/random",
+        },
+        {
+          text: "type",
+          link: "/built-in/type",
+        },
+        {
+          text: "round",
+          link: "/built-in/round",
+        },
+        {
+          text: "get_score",
+          link: "/built-in/get_score",
+        },
+        {
+          text: "set_score",
+          link: "/built-in/set_score",
+        },
+        {
+          text: "get_data",
+          link: "/built-in/get_data",
+        },
+        {
+          text: "set_data",
+          link: "/built-in/set_data",
+        },
+        {
+          text: "is_module",
+          link: "/built-in/is_module",
+        },
+        {
+          text: "연산자",
+          items: [
+            {
+              text: "multiply",
+              link: "/built-in/multiply",
+            },
+            {
+              text: "divide",
+              link: "/built-in/divide",
+            },
+          ],
+          collapsed: true,
+        },
+        {
+          text: "형변환",
+          link: "/built-in/type-exchange",
         },
       ],
     },
