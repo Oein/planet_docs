a8b09abea980f66696072909d0125df0c2445d57
Oein <oein0219@gmail.com>
2024-10-01 11:40:57 +0900

    temp

--- planet/vocs.config.ts
+++ planet/vocs.config.ts
@@ -15,5 +15,19 @@ export default defineConfig({
       text: "done 삭제하기",
       link: "/del-done",
     },
+    {
+      text: "변수",
+      link: "/variable",
+    },
+    {
+      text: "내장 함수",
+      collapsed: true,
+      items: [
+        {
+          text: "print",
+          link: "/built-in/print",
+        },
+      ],
+    },
   ],
 });
